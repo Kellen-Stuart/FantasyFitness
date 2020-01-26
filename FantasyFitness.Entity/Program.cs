dadcaf87ed147b13d9d7c4258da4feb3e322c1b9
Kellen Stuart <kellen.m.stuart@gmail.com>
2020-01-26 11:37:44 -0700

    Create the Entity Framework project

---
+++ FantasyFitness.Entity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FantasyFitness.Entity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
