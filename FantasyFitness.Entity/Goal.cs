f80bff8a65a85a11e70738917b627bde7e93c379
Kellen Stuart <kellen.m.stuart@gmail.com>
2020-02-03 20:07:26 -0700

    burned out on coding

--- FantasyFitness.Entity/Goal.cs
+++ FantasyFitness.Entity/Goal.cs
@@ -5,10 +5,10 @@ namespace FantasyFitness.Entity
     public class Goal
     {
         public Guid GoalId { get; set; }
-        public string Type { get; set; }
+        public Guid GoalTypeId { get; set; }
         public string Description { get; set; }
         public int Target { get; set; }
         public int Actual { get; set; }
-        public string Unit { get; set; }
+        public string UnitOfMeasurement { get; set; }
     }
 }
