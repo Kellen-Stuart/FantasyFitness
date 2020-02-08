a68323c260ecdd5d96ee48205a572166e647f671
Kellen Stuart <kellen.m.stuart@gmail.com>
2020-02-08 15:11:04 -0700

    adding everything that I forgot

---
+++ FantasyFitness/Migrations/20200208220630_Add_Goal_Fields_To_Match_Excel_Spreadsheet.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FantasyFitness.Migrations
+{
+    public partial class Add_Goal_Fields_To_Match_Excel_Spreadsheet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Actual",
+                table: "Goals",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Goals",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Season",
+                table: "Goals",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Target",
+                table: "Goals",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "TeamId",
+                table: "Goals",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Goals",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "Goals",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Week",
+                table: "Goals",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Actual",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "Season",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "Target",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Goals");
+
+            migrationBuilder.DropColumn(
+                name: "Week",
+                table: "Goals");
+        }
+    }
+}
---
+++ FantasyFitness/Migrations/20200208220956_Add_Team_Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FantasyFitness.Migrations
+{
+    public partial class Add_Team_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
